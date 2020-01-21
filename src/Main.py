5acc37177d25bc6a23da6d05f51518c9daecc1a7
Mohamed Serry <contact@mserry.net>
2020-01-21 19:51:25 +0400

    ** ADDED MAIN FUNCTION **

--- src/Main.py
+++ src/Main.py
@@ -3,6 +3,12 @@ import Constants as c
 from FileHandling import TextFileHandler as tfh
 from TextSummarizing import ExtractionSummarizer as es
 
-text = tfh.load_text_from_file(c.text_file_name)
-summary = es().get_summary_for_text(text)
-tfh.save_text_to_file(c.summary_title + summary, c.text_file_name)
+
+def run_text_summary_demo():
+    text = tfh.load_text_from_file(c.text_file_name)
+    summary = c.summary_title + es().get_summary_for_text(text)
+    tfh.save_text_to_file(summary, c.text_file_name)
+
+
+if __name__ == "__main__":
+    run_text_summary_demo()
