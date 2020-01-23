e1f2f5b776b08d6bd6634536fc3af6081b6d930c
mserry94 <contact@mserry.net>
2020-01-23 14:53:17 +0400

    added better qoutes for directory of data files

--- src/Constants.py
+++ src/Constants.py
@@ -1,12 +1,12 @@
 # sentence score threshold factor, hyper parameter that can be
 # tweaked that dictates the boundary on which we choose the top sentences.
-avg_score_factor = 1.24
+avg_score_factor = 1.25
 
 # percentage summary any value < 50% only
 threshold_percentage = 0.4
 
 # files directory to load from. please change this first before you proceed.
-data_files_dir = 'D:\Projects\Tests\data'
+data_files_dir = 'C:\\Users\\semap\\Downloads\\Tests-master\\Tests-master\\data'
 
 # default name for the text file. please change this if you'd require loading from a different file.
 text_file_name = "Text_To_Summarize.txt"
