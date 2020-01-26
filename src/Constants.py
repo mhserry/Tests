a873ae055a54936be5cb103e7626e1c715944581
Mohamed Serry <contact@mserry.net>
2020-01-26 18:17:57 +0400

    changed local path
    sad

--- src/Constants.py
+++ src/Constants.py
@@ -3,10 +3,10 @@
 avg_score_factor = 1.25
 
 # percentage summary any value < 50% only
-threshold_percentage = 0.4
+threshold_percentage = 0.2
 
 # files directory to load from. please change this first before you proceed.
-data_files_dir = 'C:\\Users\\semap\\Downloads\\Tests-master\\Tests-master\\data'
+data_files_dir = 'D:\\Projects\\Tests\\data'
 
 # default name for the text file. please change this if you'd require loading from a different file.
 text_file_name = "Text_To_Summarize.txt"
